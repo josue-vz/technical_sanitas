7f850fc7fbf41909a0a1475f223f446b797dd90e
JOSUE RENZO VILLACRES <josue.villacres@inetum.com>
2023-06-22 09:36:10 +0200

    Response entity VO

--- src/main/java/com/technical/test/sanitas/vo/GeneralResponseVO.java
+++ src/main/java/com/technical/test/sanitas/vo/GeneralResponseVO.java
@@ -1,13 +1,24 @@
 package com.technical.test.sanitas.vo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class GeneralResponseVO {
     private int status;
     private String message;
     private double data;
+
+    public ResponseEntity<GeneralResponseVO> success() {
+        return new ResponseEntity<>(this, HttpStatus.OK);
+    }
+
+    public ResponseEntity<GeneralResponseVO> badRequest() {
+        return new ResponseEntity<>(this, HttpStatus.BAD_REQUEST);
+    }
 }
